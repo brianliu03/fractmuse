73fcc9e5a1c24a9f7d7176bd9e42d95995d4d94b
bliu <brianliu03@gmail.com>
2023-09-16 21:05:29 -0700

    Add prototype swift

---
+++ FractMuse/FractMuse/FractMuseApp.swift
@@ -0,0 +1,17 @@
+//
+//  FractMuseApp.swift
+//  FractMuse
+//
+//  Created by Brian Liu on 9/15/23.
+//
+
+import SwiftUI
+
+@main
+struct FractMuseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
