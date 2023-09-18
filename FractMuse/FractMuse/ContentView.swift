3f05521f82d06c510ed6b032e46929eb4d5cf3ac
bliu <brianliu03@gmail.com>
2023-09-18 14:43:24 -0700

    Swiftui try

--- FractMuse/FractMuse/ContentView.swift
+++ FractMuse/FractMuse/ContentView.swift
@@ -8,12 +8,16 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        HStack {
+            VStack {
+                Text("Compositions")
+                Button("New") {
+                    print("pressed")
+                }
+            }
+            Text("Content")
         }
         .padding()
     }
