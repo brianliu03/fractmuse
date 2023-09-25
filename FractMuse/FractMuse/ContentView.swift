f8bed8c442f39d24307ab2f2f6dd5868f0dc310a
Brian Liu <64610885+brianliu03@users.noreply.github.com>
2023-09-25 16:30:17 -0700

    Update base

--- FractMuse/FractMuse/ContentView.swift
+++ FractMuse/FractMuse/ContentView.swift
@@ -22,43 +22,13 @@ struct NavBar: View {
 
     var body: some View {
         NavigationStack {
-            
+            ScrollView {
+                VStack(spacing: 20) {
+                    NavigationLink("Sample") {
+                    }
+                }
+            }
         }
-//        VStack(spacing: 0) {
-//            VStack(alignment: .leading, spacing: 0) {
-//                HStack(spacing: 0) {
-//                    HStack(spacing: 16) {
-//                        HStack(alignment: .top, spacing: 10) {
-//                            Text("􀈙")
-//                                .font(Font.custom("SF Pro", size: 17))
-//                                .lineSpacing(22)
-//                                .foregroundColor(Color(red: 0, green: 0.48, blue: 1))
-//                        }
-//                    }
-//                    .padding(EdgeInsets(top: 11, leading: 0, bottom: 11, trailing: 16))
-//                }
-//                .padding(EdgeInsets(top: 0, leading: 347, bottom: 0, trailing: 0))
-//                .frame(width: 393)
-//                VStack(alignment: .leading, spacing: 10) {
-//                    Text("Compositions")
-//                        .font(Font.custom("SF Pro", size: 34).weight(.bold))
-//                        .lineSpacing(41)
-//                        .foregroundColor(.black)
-//                }
-//                .padding(EdgeInsets(top: 3, leading: 16, bottom: 8, trailing: 16))
-//                .frame(maxWidth: .infinity, minHeight: 52, maxHeight: 52)
-//            }
-//            .frame(maxWidth: .infinity, minHeight: 96, maxHeight: 96)
-//        }
-//        .frame(width: 393, height: 150)
-//        .background(Color(red: 1, green: 1, blue: 1).opacity(0.75))
-//        .overlay(
-//            Rectangle()
-//                .inset(by: 0.17)
-//                .stroke(
-//                    Color(red: 0, green: 0, blue: 0).opacity(0.30), lineWidth: 0.17
-//                )
-//        )
     }
 }
 
